841decbde73d9cedcf5faa9c539125d43e5e965f
HanLouise23 <hannahhinkley@yahoo.com>
2025-08-08 00:43:53 +0100

    move directories

--- Untitled-1.js
+++
@@ -1,8 +0,0 @@
-function loadData() {
-    fetch('http://localhost:8000/restaurants')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('data').innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch(error => console.error('Error:', error));
-}
