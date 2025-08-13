56652d7ea938dfac753ca4b7a0cd69b448406964
HanLouise23 <hannahhinkley@yahoo.com>
2025-08-13 19:55:14 +0100

    header changes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import leftImg from '/img/left.png';
 import headerImg from '/img/header.png';
+import rightImg from '/img/right.png';
 
 const Header: React.FC = () => (
   <header className="header">
-    <img src={headerImg} alt="Restaurant Finder" />
+    <div className="header-images">
+      <img src={leftImg} alt="Left decoration" />
+      <img src={headerImg} alt="Restaurant Finder" />
+      <img src={rightImg} alt="Right decoration" />
+    </div>
   </header>
 );
 
